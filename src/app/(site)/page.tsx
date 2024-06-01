6681e8812663f13eb041e18b98d4261f50cc4175
H-SM <harmanmalht777@gmail.com>
2024-06-01 19:41:26 +0530

    fix: cloudinary support & navigation routes

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -6,13 +6,34 @@ import { useContext, useEffect, useState } from "react";
 import { useRouter } from "next/navigation"
 import { useAuthenticateConnectedUser, useDynamicContext } from "@dynamic-labs/sdk-react-core";
 import Loader from "./../components/loaderhere"
+import { useBalance } from "wagmi";
+import supabase from "../lib/supabase";
+
+interface LeaderboardEntry {
+  dynamicUserId: string;
+  username: string;
+  profilePicture: string;
+  score: number;
+}
+
 export default function Home() {
   const router = useRouter();
   const { user, isAuthenticated } = useDynamicContext();
   const { isAuthenticating } = useAuthenticateConnectedUser();
   const { settings, scores, manageUser } = useContext(ContextValue);
-
   const [loader, setLoader] = useState(1);
+
+  //TODO: look over this
+  let stringer = "";
+  if (user && user.verifiedCredentials?.[0]?.address) {
+    stringer = user.verifiedCredentials?.[0]?.address.slice(2);
+  }
+  const { data: ethBalanceData } = useBalance({
+    address: `0x${stringer}`,
+  });
+
+  const [leaderboard, setLeaderboard] = useState<LeaderboardEntry[]>([]);
+
   useEffect(() => {
     setTimeout(() => {
       if (isAuthenticated === false && isAuthenticating === false) {
@@ -28,7 +49,6 @@ export default function Home() {
             dynamicUserId: user.userId ?? "",
             picture: user.verifiedCredentials?.[2]?.oauthAccountPhotos?.[0] ?? "",
             username: user.verifiedCredentials?.[2]?.oauthUsername ?? "",
-            theme: true,
             multiplier: 1,
             netWorth: 0,
             totalWorth: 0,
@@ -44,132 +64,106 @@ export default function Home() {
     }, 1000)
   }, []);
 
+  const fetchData = async () => {
+    const response = await fetch('/api/leaderboard');
+    const data = await response.json();
+    if (data) {
+      setLeaderboard(data);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const channel = supabase
+      .channel('leaderboard showcase realtime')
+      .on('postgres_changes', {
+        event: "*",
+        schema: "public",
+        table: "UserScores"
+      }, () => {
+        console.log('Change received!');
+        fetchData();
+      }).subscribe();
+
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, []);
+
+
   return (
     <>
       {loader === 1 ?
         <Loader />
         :
         <>
-          <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-              <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-                Get started by editing&nbsp;
-                {/* <code className="font-mono font-bold">OUR THEME : {theme === true ? "dark" : "light"}</code> */}
+          <div>
+            <div className="flex justify-center flex-col items-center">
+              <p>
+                {settings.username}
               </p>
-              {/* <button type="button" onClick={toggleTheme}>ToggleTheme</button> */}
-              <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-                <a
-                  className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-                  href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  By{" "}
-                  <Image
-                    src="/vercel.svg"
-                    alt="Vercel Logo"
-                    className="dark:invert"
-                    width={100}
-                    height={24}
-                    priority
-                  />
-                </a>
-              </div>
+              <p>
+                USERS NET WORTH
+              </p>
+              <p>
+                {scores.multiplier * scores.netWorth}
+              </p>
             </div>
-            <div>
-              <div className="flex justify-center flex-col items-center">
-                <p>
-                  {settings.username}
-                </p>
-                <p>
-                  USERS NET WORTH
-                </p>
-                <p>
-                  {scores.multiplier * scores.netWorth}
-                </p>
-              </div>
-            </div>
-            <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-              <Image
-                className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-                src="/next.svg"
-                alt="Next.js Logo"
-                width={180}
-                height={37}
-                priority
-              />
-            </div>
+          </div>
+          <div>
+            {isAuthenticated === false ?
+              <div>LOADING...</div>
+              :
+              user ? (
+                <div>
+                  <p>Issuer: {user.userId}</p>
+                  <p>email: {user.email}</p>
+                  <p>lastVerifiedCredentialId: {user.lastVerifiedCredentialId}</p>
+                  <p>environmentId: {user.environmentId}</p>
+                  <p>username: {settings.username}</p>
+                </div>
+              ) : (
+                <p>No verified credentials available</p>
+              )}
 
-            <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-              <a
-                href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <h2 className="mb-3 text-2xl font-semibold">
-                  Docs{" "}
-                  <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                    -&gt;
-                  </span>
-                </h2>
-                <p className="m-0 max-w-[30ch] text-sm opacity-50">
-                  Find in-depth information about Next.js features and API.
-                </p>
-              </a>
-
-              <a
-                href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <h2 className="mb-3 text-2xl font-semibold">
-                  Learn{" "}
-                  <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                    -&gt;
-                  </span>
-                </h2>
-                <p className="m-0 max-w-[30ch] text-sm opacity-50">
-                  Learn about Next.js in an interactive course with&nbsp;quizzes!
-                </p>
-              </a>
-
-              <a
-                href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <h2 className="mb-3 text-2xl font-semibold">
-                  Templates{" "}
-                  <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                    -&gt;
-                  </span>
-                </h2>
-                <p className="m-0 max-w-[30ch] text-sm opacity-50">
-                  Explore starter templates for Next.js.
-                </p>
-              </a>
-
-              <a
-                href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <h2 className="mb-3 text-2xl font-semibold">
-                  Deploy{" "}
-                  <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                    -&gt;
-                  </span>
-                </h2>
-                <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-                  Instantly deploy your Next.js site to a shareable URL with Vercel.
-                </p>
-              </a>
-            </div>
-          </main>
+            {scores.totalWorth}
+            {ethBalanceData ? <p>ETH Balance (wagmi): {ethBalanceData.formatted}</p> : <p>Loading ETH balance...</p>}
+            {/* <h3>Native Balance: {nativeBalance?.balance.ether} ETH</h3>  */}
+          </div>
+          <div className="container mx-auto p-4">
+            <h1 className="text-2xl font-bold mb-4">Leaderboard</h1>
+            <table className="min-w-full bg-blue-400">
+              <thead>
+                <tr>
+                  <th className="py-2 px-4 border-b">Username</th>
+                  <th className="py-2 px-4 border-b">Profile Picture</th>
+                  <th className="py-2 px-4 border-b">Net Worth</th>
+                </tr>
+              </thead>
+              <tbody>
+                {leaderboard.map((entry, index) => (
+                  <tr key={index}>
+                    <td className="py-2 px-4 border-b">{entry.username}</td>
+                    <td className="py-2 px-4 border-b">
+                      {entry.profilePicture ? (
+                        <img
+                          src={entry.profilePicture}
+                          alt="Profile"
+                          className="w-8 h-8 rounded-full"
+                        />
+                      ) : (
+                        <p>No image</p>
+                      )}
+                    </td>
+                    <td className="py-2 px-4 border-b">{entry.score.toFixed(2)}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </>
       }
     </>
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -6,6 +6,7 @@ import { useAuthenticateConnectedUser, useDynamicContext } from '@dynamic-labs/s
 import { useRouter } from "next/navigation";
 import Loader from "./../components/loaderhere"
 import { UserSettings } from '@prisma/client';
+import { CldUploadWidget } from 'next-cloudinary';
 
 const Settings = () => {
   const router = useRouter();
@@ -60,6 +61,14 @@ const Settings = () => {
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
     setDetails({ ...details, [e.target.name]: e.target.value });
   }
+
+  const handleImageUpload = (result: any) => {
+    const imageUrl = result.info.secure_url;
+    setDetails((prevDetails) => ({
+      ...prevDetails,
+      picture: imageUrl,
+    }));
+  };
 
   return (
     <>
@@ -134,44 +143,58 @@ const Settings = () => {
                               aria-hidden="true"
                             />
                           )}
-                          {/* <CloudinaryUploadWidget showAlert={showAlert} /> */}
-                        </div>
+                          <CldUploadWidget uploadPreset="ppgzrcox" onSuccess={handleImageUpload}
+                          >
+                            {({ open }) => {
+                              return (
+                                <button
+                                  onClick={(event) => {
+                                    event.preventDefault();
+                                    open(); 
+                                  }}
+                                  className='rounded-md bg-cyan-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600'>
+                                  Upload an Image
+                                </button>
+                          );  
+                            }}
+                        </CldUploadWidget>
                       </div>
                     </div>
-
-
-                    <label className="inline-flex items-center cursor-pointer">
-                      <input type="checkbox"
-                        checked={theme === true}
-                        onChange={() => {
-                          toggleTheme();
-                          console.log(theme);
-                        }
-                      }
-                        className="sr-only peer" />
-                      <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                      <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Dark Mode</span>
-                    </label>
                   </div>
+
+
+                  <label className="inline-flex items-center cursor-pointer">
+                    <input type="checkbox"
+                      checked={theme === true}
+                      onChange={() => {
+                        toggleTheme();
+                        console.log(theme);
+                      }
+                      }
+                      className="sr-only peer" />
+                    <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                    <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Dark Mode</span>
+                  </label>
                 </div>
-                <div className="mt-6 flex items-center justify-end gap-x-6 w-full">
-                  <button
-                    type="button"
-                    className="text-sm font-semibold leading-6"
-                    onClick={() => setDetails(settings)}
-                  >
-                    Cancel
-                  </button>
-                  <button
-                    type="submit"
-                    className="rounded-md bg-cyan-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
-                  >
-                    Save
-                  </button>
-                </div>
-              </form>
             </div>
-          </div>
+            <div className="mt-6 flex items-center justify-end gap-x-6 w-full">
+              <button
+                type="button"
+                className="text-sm font-semibold leading-6"
+                onClick={() => setDetails(settings)}
+              >
+                Cancel
+              </button>
+              <button
+                type="submit"
+                className="rounded-md bg-cyan-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-600"
+              >
+                Save
+              </button>
+            </div>
+          </form>
+        </div>
+    </div >
         </>
       }
     </>
