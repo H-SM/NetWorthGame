a3b97ebb9edcebd87875ab756ecd261bbbdc7ece
H-SM <harmanmalht777@gmail.com>
2024-06-02 18:47:47 +0530

    fix(deploy): minor vercel fixes - 2

--- src/app/api/wallet/route.ts
+++ src/app/api/wallet/route.ts
@@ -2,25 +2,23 @@ import { NextRequest, NextResponse } from 'next/server';
 import { initMoralis } from '../../lib/moralis';
 import Moralis from 'moralis';
 
-export async function POST(request: NextRequest, response: NextResponse) {
+export async function POST(request: NextRequest) {
   try {
     await initMoralis();
     const body = await request.json();
     let { address } = body;
 
-    const response = await Moralis.EvmApi.wallets.getWalletTokenBalancesPrice({
+    const moralisResponse = await Moralis.EvmApi.wallets.getWalletTokenBalancesPrice({
       chain: '0x1',
       address
     }); 
 
-    console.log(response);
-    return NextResponse.json(response);
+    console.log(moralisResponse);
+    return NextResponse.json(moralisResponse);
   } catch (error) {
     console.error(error);
     return new NextResponse(JSON.stringify({ error: "error while getting getWalletTokenBalancesPrice" }), { status: 500 });
   }
 }
 
-export const config = {
-  runtime: 'edge',
-};
+export const runtime = 'edge';
